53d7cc6ad2652c0350ced4ca301bb5a5b5c0a17d
Vaibhav Singh <vaibhavpratapsingh22@gmail.com>
2023-03-23 22:35:40 +0530

    complete
    Simple search with filter method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-
+import './App.css'
+import { useState } from 'react';
+import { Users } from './users';
+import Table from './Table';
 function App() {
+  const [searchUser, setSearchUser] = useState('');
+  const handleInput = (e)=>{
+    setSearchUser(e.target.value);
+  }
   return (
     <div className="App">
-      Hello
+     <input type="text" placeholder="search" onChange={(e)=>handleInput(e)} className="search" />
+     <ul className="list">
+      {Users.filter(user=>user.first_name.toLocaleLowerCase().includes(searchUser)).map((user)=>{
+      return(
+      <li className="listItem" key={user.id}>{user.first_name}</li>)  
+      })}
+     </ul>
+     <Table />
     </div>
   );
 }
